869cbfe2e555e9579844df3c24383e570ebe90a3
吕树伟 <2676727918@qq.com>
2022-12-29 08:37:33 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <loveHeart msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import LoveHeart from './components/love-heart.vue'
+
+export default {
+  name: 'app',
+  components: {
+    LoveHeart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/love-heart.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="heart">
+	<div class="love" v-for="item in 100" :style="{'--d':(item*-1)*0.5}">
+		<div class="love-leng">
+			<div class="love-cro">
+				<div class="love-text">吕先生爱着高女士</div>
+			</div>
+		</div>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'love-heart',
+  data() {
+	  return {
+		  
+	  }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	@import "./love.css"
+</style>
